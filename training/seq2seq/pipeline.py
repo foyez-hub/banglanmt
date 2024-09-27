392efc67efdcfe3510b7ae1ff3dbcafe4b46cdd7
foyez-hub <59407058+foyez-hub@users.noreply.github.com>
2024-09-27 12:34:45 +0600

    Update pipeline.py

--- training/seq2seq/pipeline.py
+++ training/seq2seq/pipeline.py
@@ -229,7 +229,7 @@ def main(args):
     if args.do_eval:
         evaluate(args)
     
-    wandb.finish()                       
+                           
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
